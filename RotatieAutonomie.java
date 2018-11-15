944f39538cb488cf63745b2d2ea60e220b18a9b6
BNGCNLR <39764844+BNGCNLR@users.noreply.github.com>
2018-11-15 16:26:11 +0200

    Create RotatieAutonomie.java

---
+++ RotatieAutonomie.java
@@ -0,0 +1,84 @@
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name="Rotatie Autonomie", group="Linear Opmode")
+public class RotatieAutonomie extends LinearOpMode {
+
+    private Robot robot;
+    private DcMotor motorLeft1 = null;
+    private DcMotor motorRight1 = null;
+    private DcMotor motorLeft2 = null;
+    private DcMotor motorRight2 = null;
+
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+
+        Init();
+        waitForStart();
+
+        robot.rotate(360);
+        robot.rotate(-360);
+
+    }
+
+    public void Init()
+    {
+
+        robot = new Robot();
+        motorLeft1 = hardwareMap.dcMotor.get("motorLeft1");
+        motorLeft2 = hardwareMap.dcMotor.get("motorLeft2");
+        motorRight1 = hardwareMap.dcMotor.get("motorRight1");
+        motorRight2 = hardwareMap.dcMotor.get("motorRight2");
+
+        telemetry.log().add("Init completed");
+
+    }
+
+    public class Robot
+    {
+
+        private Robot(){}
+
+        public void rotate(int degrees)
+        {
+
+            int ticks = toTicks(degrees);
+
+            motorLeft1.setDirection(DcMotor.Direction.FORWARD);
+            motorLeft2.setDirection(DcMotor.Direction.FORWARD);
+            motorRight1.setDirection(DcMotor.Direction.FORWARD);
+            motorRight2.setDirection(DcMotor.Direction.FORWARD);
+            setMotorSpeed(ticks > 0 ? 0.4f : -0.4f);
+
+            int startPos = Math.abs(motorRight1.getCurrentPosition());
+            while (ticks > 0 ? (Math.abs(motorRight1.getCurrentPosition()) < ticks + startPos) : (Math.abs(motorRight1.getCurrentPosition()) > startPos - Math.abs(ticks)))
+            {
+                telemetry.addData("Ticks", Math.abs(motorRight1.getCurrentPosition()));
+                telemetry.addData("Goal", ticks);
+                telemetry.update();
+            }
+
+            setMotorSpeed(0f);
+            sleep(350);
+
+        }
+
+    }
+
+    public int toTicks(int degrees)
+    {
+            int maxTick = degrees >= 0 ? 3050 : 1700;
+            return maxTick * degrees / 360;
+
+    }
+
+    private void setMotorSpeed(float speed)
+    {
+        motorLeft2.setPower(speed);
+        motorRight2.setPower(speed);
+        motorLeft1.setPower(speed);
+        motorRight1.setPower(speed);
+    }
+}
